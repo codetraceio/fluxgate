4ea5e9fd04ccdf6d9cb3c5f21557a244dc8bd575
omegascorp <omegascorp@gmail.com>
2019-06-01 10:48:03 +0400

    Update store.

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,12 @@
-import { createEmitter } from "./emitter";
 import { IStore, IEmitter, IReducerMap, IActionMap } from "./interfaces";
 
 export const EVENT_CHANGE = "change";
 
-export function createStore<S extends {[key: string]: any}>(defaultState: S): IStore<S> {
-  const emitter = createEmitter();
+export function createStore<S extends {[key: string]: any}>(
+  emitter: IEmitter,
+  defaultState: S,
+): IStore<S> {
+
   let state = {...defaultState};
 
   function setState(updater: S | ((state: S) => S)) {
@@ -30,17 +32,36 @@ export function createStore<S extends {[key: string]: any}>(defaultState: S): IS
   };
 }
 
-function createAction(emitter: IEmitter, key: string, reducer: (...data: any[]) => void) {
-  emitter.on(key, reducer);
+function createAction<T>(
+  store: IStore<T>,
+  key: string,
+  reducer: (...data: any[]) => void | Promise<any>,
+) {
+  store.on(key, (...data: any[]) => {
+    const promise = reducer(...data);
+    if (promise instanceof Promise) {
+      store.emit(`${key}.loading`);
+      promise.then(() => {
+        store.emit(`${key}.done`);
+      });
+    } else {
+      store.emit(`${key}.done`);
+    }
+  });
   return (...data: any[]) => {
-    emitter.emit(key, data);
+    store.emit(key, data);
+    return new Promise((resolve) => {
+      store.on(`${key}.done`, () => {
+        resolve();
+      });
+    });
   };
 }
 
-export function createActions<T extends IReducerMap>(emitter: IEmitter, reducerMap: T): IActionMap<T> {
+export function createActions<T extends IReducerMap, S>(store: IStore<S>, reducerMap: T): IActionMap<T> {
   const actionMap: IActionMap<T> = {} as IActionMap<T>;
   Object.keys(reducerMap).forEach((key: string) => {
-    actionMap[key] = createAction(emitter, key, reducerMap[key]);
+    actionMap[key] = createAction(store, key, reducerMap[key]);
   });
   return actionMap;
 }
